6dfc6e8f44fbfa937ef75933001951a4585bf690
diesen <diesen.aruzu65@gmail.com>
2020-12-15 05:00:55 +0900

    bug fix

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def handle_location(event):
 
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=response.json()))
+        TextSendMessage(text=response.text))
 
 
 #友達追加時イベント
